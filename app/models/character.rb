1e7fbac463ac3a5dbe2a2ef0260bc16e0d6c91c3
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-01 12:41:22 +0300

    Completed

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,28 @@
 class Character < ActiveRecord::Base
+    belongs_to :show
+    belongs_to :actor
+    
+     def show
+      self.show_id ? Show.find(self.show_id) : nil
+    end
+
+
+
+    def actor
+      self.actor_id ? Actor.find(self.actor_id) : nil
+    end
+
+    def build_show(name:)
+        self.show = Show.create(name: name)
+    end
+
+
+    def show
+        self.show_id ? Show.find(self.show_id) : nil
+    end
+
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end
   
 end
